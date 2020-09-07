64bb5460f9a6ee7c7e70fb52d4711f512f18ae40
David Lee <dlee6018@gmail.com>
2020-09-07 17:10:56 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <h1>This is David</h1>
+    </div>
+  );
+}
+
+export default App;
